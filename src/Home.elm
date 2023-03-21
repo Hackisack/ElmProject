ca3e38a0faaf956c7467bd49f84ace2eb3b43e0e
Vinzenz Liebherr <vinzenz.liebherr@hs-furtwangen.de>
2023-03-21 22:14:15 +0100

    Fetch DBData and create new Name

--- src/Home.elm
+++ src/Home.elm
@@ -2,13 +2,24 @@ module Home exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Random
 import Random exposing (Generator)
 
 import Random.Char
 import Random.String exposing (string)
+import Http
+import Http exposing (Body)
+import Http exposing (request)
+import Http exposing (expectStringResponse)
+import Random.Extra exposing (maybe)
+import Random exposing (Generator)
+import Random exposing (Generator)
+import Random exposing (Generator)
+import Maybe exposing (Maybe)
+import Http
+import Http exposing (Header)
 
 
 
@@ -31,13 +42,14 @@ main =
 
 type alias Model =
     { value : Int
-    , rString : String
+    , randomString : String
+    , responseString : String
      }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    (Model 1 "" , Cmd.none )
+    (Model 1 "" "", Cmd.none )
 
 
 
@@ -46,17 +58,24 @@ init _ =
 
 type Msg
     = Rolled String
-    | NewString
+    | HTTPRequest
+    | HTTPResponse (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Rolled newValue ->
-            ( { model | rString = newValue }, Cmd.none )
+            ( { model | randomString = newValue }, Cmd.none )
 
-        NewString ->
-            ( model, Random.generate Rolled (fiveLetterEnglishWord) )
+        HTTPRequest ->
+            ( model, getData)
+
+        HTTPResponse (Ok response) ->
+            ( { model | responseString = response },  Random.generate Rolled (fiveLetterEnglishWord) )
+
+        HTTPResponse (Err error) ->
+            ( { model | responseString =  "An Error Occured"}, Cmd.none )
 
 
 
@@ -75,11 +94,26 @@ subscriptions _ =
 view : Model -> Html Msg
 view model =
     div [ ]
-        [ div [] [ button [ onClick NewString ] [ text "Roll The Dice" ] ]
-        , div [] [text(model.rString)]
+        [ div [] [ button [onClick HTTPRequest] [ text "Create New Room" ] ]
+        , div [] [text(model.randomString)]
+        , div [] [text(model.responseString)]
         ]
 
 
 fiveLetterEnglishWord : Generator String
 fiveLetterEnglishWord =
      string 5 Random.Char.english
+
+
+getData : Cmd Msg
+getData =
+    Http.request
+    {method = "GET"
+    , headers = [Http.header "X-Parse-Application-Id" "58G7kMmJiXqTEW6MCENwiLb6H8ebaiCJX3ahL91c", Http.header "X-Parse-REST-API-Key" "elB9iy4qqTAHzWxdQtFTqRsm84tTRctjyAmMyIBO"]
+    , url = "https://parseapi.back4app.com/classes/RoomEntry"
+    , body = Http.emptyBody
+    , expect = Http.expectString HTTPResponse
+    , timeout = Nothing
+    , tracker = Nothing
+    }
+   
