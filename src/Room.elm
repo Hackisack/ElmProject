afce584c2fbdbff7e5ad585ecad55487d0e155fb
Vinzenz Liebherr <vinzenz.liebherr@hs-furtwangen.de>
2023-03-30 16:41:37 +0200

    Save push

--- src/Room.elm
+++ src/Room.elm
@@ -9,6 +9,9 @@ import Url
 import Url.Parser as Parser
 import Url.Parser.Query as Query
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder)
+import Json.Decode exposing (field)
 
 
 
@@ -29,12 +32,27 @@ type alias Model =
     , url : Url.Url
     , property : String
     , roomID : String
+    , room : MyObject
+    , error : String
     }
+
+type alias MyObject =
+    { objectId : String
+    , specifiedDates : List String
+    , users : List String
+    , acceptedDates : List String
+    , roomName : String
+    , createdAt : String
+    , updatedAt : String
+    }
+
+type alias MyResults =
+    { results :List MyObject }
 
 
 init : flags -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url "modelInitialValue" "", Cmd.none )
+    ( Model key url "modelInitialValue" ""  { objectId = "", specifiedDates = [""], users = [""], acceptedDates = [""], roomName = "", createdAt ="", updatedAt = ""} "", Cmd.none )
 
 
 type Msg
@@ -42,7 +60,8 @@ type Msg
     | Msg2
     | UrlRequested Browser.UrlRequest
     | UrlChanged Url.Url
-    | Test
+    | RetrieveUrlID
+    | GotData (Result Http.Error MyResults)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -66,12 +85,18 @@ update msg model =
             , Cmd.none
             )
 
-        Test ->
-            ( {model | roomID =Maybe.withDefault "Error" (List.head (List.reverse (String.split "=" (Url.toString model.url))))}, Cmd.none)
+        RetrieveUrlID ->
+            ( {model | roomID =Maybe.withDefault "Error" (List.head (List.reverse (String.split "=" (Url.toString model.url))))}, getData model)
+
+        GotData (Ok response) ->
+           ( { model | room =(List.head (response.results))}, Cmd.none )
+            
+        GotData (Err _) ->
+            ( { model | error = "Error" }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
 
 
@@ -79,9 +104,55 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Room"
     , body =
-        [ div [] [ button [onClick Test] [ text "Create New Room" ] ]
-         ,div [] [ text (model.roomID) ]
+        [ div [] [ button [onClick RetrieveUrlID] [ text "Join this Room" ] ]
+          ,div [] [ text (model.roomID) ]
+          ,div [] [ text (Debug.toString model.room) ]
         ]
     }
 
+-- Placeholders
 
+defaultResults : MyResults
+defaultResults =
+    { results = [defaultObject ] }
+
+defaultObject : MyObject
+defaultObject =
+    { objectId = ""
+    , specifiedDates = [""]
+    , users = [""]
+    , acceptedDates = [""]
+    , roomName = ""
+    , createdAt =""
+    , updatedAt = ""
+    }
+
+-- Functions
+
+getData :Model -> Cmd Msg
+getData model=
+    Http.request
+    {method = "GET"
+    , headers = [Http.header "X-Parse-Application-Id" "58G7kMmJiXqTEW6MCENwiLb6H8ebaiCJX3ahL91c", Http.header "X-Parse-REST-API-Key" "elB9iy4qqTAHzWxdQtFTqRsm84tTRctjyAmMyIBO"]
+    , url = "https://parseapi.back4app.com/classes/RoomEntry"
+    , body = Http.stringBody ("where={\"roomID\":\""           ++ model.roomID ++             "\"}") "application/json"
+    , expect = Http.expectJson GotData myResultsDecoder
+    , timeout = Nothing
+    , tracker = Nothing
+    }
+
+myObjectDecoder : Decoder MyObject
+myObjectDecoder =
+    Json.Decode.map7 MyObject
+        (field "objectId" Json.Decode.string)
+        (field "specifiedDates" (Json.Decode.list Json.Decode.string))
+        (field "users" (Json.Decode.list Json.Decode.string))
+        (field "acceptedDates" (Json.Decode.list Json.Decode.string))
+        (field "roomName" Json.Decode.string)
+        (field "createdAt" Json.Decode.string)
+        (field "updatedAt" Json.Decode.string)
+
+myResultsDecoder : Decoder MyResults
+myResultsDecoder =
+    Json.Decode.map MyResults
+        (field "results" (Json.Decode.list myObjectDecoder))
